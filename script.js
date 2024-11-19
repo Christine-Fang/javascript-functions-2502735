dbdee701cf35e61c539e864182ee59226e677a22
Christine Fang <154557684+Christine-Fang@users.noreply.github.com>
2024-11-19 21:57:56 +0000

    hoisting gives unexpected var value

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ var temp = 86
 tempC()
 console.log(temp) // 86
 function tempC() {
+    //var temp
     console.log(temp) // undefined
+    //temp = 50
     var temp = 50
     temp = (temp - 32) / 1.8
     console.log(temp) // 10
 }
+
+//hoisting
+//after compilation, line 4-9 function declaration will be moved to before invocation on line 3
+//inside tempC, line 8 is broken into 2 parts
+//declaration is moved to before logging temp and replaces the global temp, which gives undefined
+//outside tempC, it's the global temp
